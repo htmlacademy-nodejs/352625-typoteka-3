47bc96e556e0691e21a43a623db24e8f0a8d8579
twelwet <akimovmax@mail.ru>
2020-05-13 09:57:28 +0300

    Добавлена директория с ресурсами

--- src/express/index.js
+++ src/express/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const {DEFAULT_PORT} = require(`./../service/cli/constants.js`);
 const PathName = require(`./routes/constants.js`);
@@ -13,10 +14,14 @@ const offersRouter = require(`./routes/offers.js`);
 const myRouter = require(`./routes/my.js`);
 const errorRouter = require(`./routes/error.js`);
 
+const PUBLIC_DIR = `public`;
+
 const app = express();
 
 app.set(`views`, `./src/express/templates`);
 app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, homeRouter);
 app.use(`/${PathName.REGISTER}`, registerRouter);
