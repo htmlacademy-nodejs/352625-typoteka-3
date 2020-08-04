eb117d8e7e04c785fde93c55f48196c9c3831602
twelwet <akimovmax@mail.ru>
2020-08-04 16:31:19 +0300

    sometimes .find better than .filter

--- src/service/routes/articles.js
+++ src/service/routes/articles.js
@@ -47,7 +47,7 @@ articlesRouter.get(`/:articleId`, async (req, res) => {
   try {
     const data = await getMock();
     const result = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!result) {
       res.status(HttpCode.BAD_REQUEST).json(Empty.ARTICLE);
@@ -66,7 +66,7 @@ articlesRouter.get(`/:articleId/comments`, async (req, res) => {
   try {
     const data = await getMock();
     const targetArticle = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!targetArticle) {
       res.status(HttpCode.BAD_REQUEST).json(Empty.COMMENTS);
@@ -105,7 +105,7 @@ articlesRouter.put(`/:articleId`, async (req, res) => {
   try {
     const data = await getMock();
     const result = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!result) {
       res.status(HttpCode.BAD_REQUEST).send(Empty.ARTICLE);
@@ -125,7 +125,7 @@ articlesRouter.put(`/:articleId/comments`, async (req, res) => {
   try {
     const data = await getMock();
     const result = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!validateComment() || !result) {
       res.status(HttpCode.BAD_REQUEST).send(Empty.COMMENT);
@@ -145,7 +145,7 @@ articlesRouter.delete(`/:articleId`, async (req, res) => {
   try {
     const data = await getMock();
     const result = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!result) {
       res.status(HttpCode.BAD_REQUEST).send(`Invalid Article ID`);
@@ -165,14 +165,14 @@ articlesRouter.delete(`/:articleId/comments/:commentId`, async (req, res) => {
   try {
     const data = await getMock();
     const targetArticle = data
-      .filter((elem) => elem.id === req.params.articleId)[0];
+      .find((elem) => elem.id === req.params.articleId);
 
     if (!targetArticle) {
       res.status(HttpCode.BAD_REQUEST).send(`Invalid article ID`);
 
     } else {
       const targetComment = targetArticle.comments
-        .filter((elem) => elem.id === req.params.commentId)[0];
+        .find((elem) => elem.id === req.params.commentId);
 
       if (!targetComment) {
         res.status(HttpCode.BAD_REQUEST).send(`Invalid comment ID`);
