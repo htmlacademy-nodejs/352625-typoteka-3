4cef15561554a9c3541f50e70fd61ebeb9961d41
twelwet <akimovmax@mail.ru>
2020-09-29 12:45:32 +0300

    dotenv migrated from package.json to db-connect.js

--- src/service/cli/db-connect.js
+++ src/service/cli/db-connect.js
@@ -8,6 +8,8 @@ const logger = getLogger();
 
 const Sequelize = require(`sequelize`);
 
+require(`dotenv`).config();
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
