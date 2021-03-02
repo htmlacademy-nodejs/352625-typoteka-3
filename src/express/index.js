ca20c9e754bd67ce989d1fd2fab5f76de63f9e7f
twelwet <akimovmax@mail.ru>
2021-03-02 14:26:01 +0300

    Add some realtime features using socket.io

--- src/express/index.js
+++ src/express/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const express = require(`express`);
 const sessionMiddleware = require(`./session-store.js`);
 const path = require(`path`);
-const http = require(`http`);
-const socketIO = require(`socket.io`);
+const {express, app, server} = require(`./server.js`);
 require(`dotenv`).config();
 
 const pino = require(`pino`)(`./src/express/logs/express.log`);
@@ -30,10 +28,6 @@ const {getLogger} = require(`./../service/logger.js`);
 
 const logger = getLogger();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
 app.use(sessionMiddleware);
 
 app.set(`views`, `./src/express/templates`);
@@ -57,16 +51,6 @@ app.use((req, res) => {
 });
 
 app.use(expressPino);
-
-io.on(`connection`, (socket) => {
-  // TODO Не могу разобраться почему в консоль не выводятся соответсвующие сообщения. Наверное что-то не донастроил.
-  const {address: ip} = socket.handshake;
-  console.log(`Новое подключение: ${ip}`);
-
-  socket.on(`disconnect`, () => {
-    console.log(`Клиент отключён: ${ip}`);
-  });
-});
 
 server.listen(
     DEFAULT_PORT,
